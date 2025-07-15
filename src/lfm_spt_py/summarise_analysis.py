34f89b585ed9d612d0e57419e1d30baa74844829
Diyan Jain <diyanjain@gmail.com>
2025-07-15 04:35:24 +0100

    small change

--- src/lfm_spt_py/summarise_analysis.py
+++ src/lfm_spt_py/summarise_analysis.py
@@ -141,7 +141,7 @@ def anomalous_exp(matlab_results_dir):
 
 
 
-def compile_stats(sample_name, root_directory, matlab_results_dir=None, custom_dir=None):
+def compile_stats(sample_name, root_directory, matlab_results_dir=None, custom_dir=None, only_matlab=False):
     """ 
     Compiles statistics (number of 2D locs, 3D locs, tracks, confPerc) per dataset into a summary CSV file.
     Args:
@@ -149,112 +149,133 @@ def compile_stats(sample_name, root_directory, matlab_results_dir=None, custom_d
         root_directory (str): Path to root_directory to read the data folders and saving the summary stats.
         matlab_results_dir (str, optional): Directory containing MATLAB results to include % trajs confined in summary
         custom_data_dir (str, optional): replaces 'root_dir/data' with custom directory path containing the '2D_locs_csv', 'formatted_3Dlocs', 'cropped_3Dlocs', 'tracks' folders for the given sample.  
+        only_matlab (bool, optional): set to True if no data dir input, just confPerc, diffusionConst and alpha.
     Returns:
         str: Path to the generated summary CSV file.
     """
-    if custom_dir:
-        print(f'Reading subfolders, "2D_locs_csv","formatted_3Dlocs","cropped_3Dlocs","tracks", within the custom directory as provided to generate analysis summary stats...\n(note: ensure the mentioned subfolders all contain datasets only corresponding to "{sample_name}")')
-        locs2D_dir = os.path.join(custom_dir,'2D_locs_csv')
-        fm_3Dlocs_dir = os.path.join(custom_dir,'formatted_3Dlocs')
-        cropped_3Dlocs_dir = os.path.join(custom_dir, 'cropped_3Dlocs')
-        tracks_dir = os.path.join(custom_dir, 'tracks')
+    if not only_matlab:
+        if custom_dir:
+            print(f'Reading subfolders, "2D_locs_csv","formatted_3Dlocs","cropped_3Dlocs","tracks", within the custom directory as provided to generate analysis summary stats...\n(note: ensure the mentioned subfolders all contain datasets only corresponding to "{sample_name}")')
+            locs2D_dir = os.path.join(custom_dir,'2D_locs_csv')
+            fm_3Dlocs_dir = os.path.join(custom_dir,'formatted_3Dlocs')
+            cropped_3Dlocs_dir = os.path.join(custom_dir, 'cropped_3Dlocs')
+            tracks_dir = os.path.join(custom_dir, 'tracks')
+        else:
+            print(f'Reading subfolders, "2D_locs_csv","formatted_3Dlocs","cropped_3Dlocs","tracks", within the "../data" directory to generate analysis summary stats...\n(note: ensure the mentioned subfolders all contain datasets only corresponding to "{sample_name}")')
+            locs2D_dir = os.path.join(root_directory, 'data', '2D_locs_csv')
+            fm_3Dlocs_dir = os.path.join(root_directory, 'data', 'formatted_3Dlocs')
+            cropped_3Dlocs_dir = os.path.join(root_directory, 'data', 'cropped_3Dlocs')
+            tracks_dir = os.path.join(root_directory, 'data', 'tracks')
+
+        if matlab_results_dir:
+            df = pd.DataFrame({
+                'numFrames': numFrames(locs2D_dir),
+                'num_2Dlocs': num2D_locs(locs2D_dir),
+                'num_raw3Dlocs': num3D_raw_locs(fm_3Dlocs_dir),
+                'num_cropped3Dlocs': num3D_cropped_locs(cropped_3Dlocs_dir),
+                'numTracks': numTracks(tracks_dir),
+                'confPerc': perfConf(matlab_results_dir),
+                'diffusionConst (all)': diffConst(matlab_results_dir),
+                'alpha (all)': anomalous_exp(matlab_results_dir)
+            }).T
+        else: 
+            df = pd.DataFrame({
+                'numFrames': numFrames(locs2D_dir),
+                'num_2Dlocs': num2D_locs(locs2D_dir),
+                'num_raw3Dlocs': num3D_raw_locs(fm_3Dlocs_dir),
+                'num_cropped3Dlocs': num3D_cropped_locs(cropped_3Dlocs_dir),
+                'numTracks': numTracks(tracks_dir),
+            }).T
+
+        df.index.name = 'Property'
+        df = df.T
+        df.index.name = 'Sample'
+
+        # calculate per-frame values and add them as new columns
+        df['num_2Dlocs_perframe'] = df['num_2Dlocs'] / df['numFrames']
+        df['num_raw3Dlocs_perframe'] = df['num_raw3Dlocs'] / df['numFrames']
+        df['num_cropped3Dlocs_perframe'] = df['num_cropped3Dlocs'] / df['numFrames']
+        df['numTracks_perframe'] = df['numTracks'] / df['numFrames']
+
+        if matlab_results_dir:
+            destination_dir = os.path.join(root_directory, 'results', sample_name + '_perFOV_results', 'analysisSummary')
+        else:
+            destination_dir = os.path.join(root_directory, 'results', sample_name + '_analysisSummary')
+        os.makedirs(destination_dir, exist_ok=True)
+        csv_destination_path = os.path.join(destination_dir, f'{sample_name}_stats.csv')
+        df.to_csv(csv_destination_path)
+
+        # Per-frame and filtered stats
+        valid_2D = df.dropna(subset=['numFrames', 'num_2Dlocs'])
+        mean_2Dlocs = valid_2D['num_2Dlocs'].mean()
+        std_2Dlocs = valid_2D['num_2Dlocs'].std()
+        mean_2Dlocs_pf = (valid_2D['num_2Dlocs'] / valid_2D['numFrames']).mean()
+        std_2Dlocs_pf = (valid_2D['num_2Dlocs'] / valid_2D['numFrames']).std()
+
+        valid_raw3D = df.dropna(subset=['numFrames', 'num_raw3Dlocs'])
+        mean_raw3D = valid_raw3D['num_raw3Dlocs'].mean()
+        std_raw3D = valid_raw3D['num_raw3Dlocs'].std()
+        mean_raw3D_pf = (valid_raw3D['num_raw3Dlocs'] / valid_raw3D['numFrames']).mean()
+        std_raw3D_pf = (valid_raw3D['num_raw3Dlocs'] / valid_raw3D['numFrames']).std()
+
+        valid_crop3D = df.dropna(subset=['numFrames', 'num_cropped3Dlocs'])
+        mean_crop3D = valid_crop3D['num_cropped3Dlocs'].mean()
+        std_crop3D = valid_crop3D['num_cropped3Dlocs'].std()
+        mean_crop3D_pf = (valid_crop3D['num_cropped3Dlocs'] / valid_crop3D['numFrames']).mean()
+        std_crop3D_pf = (valid_crop3D['num_cropped3Dlocs'] / valid_crop3D['numFrames']).std()
+
+        valid_tracks = df.dropna(subset=['numFrames', 'numTracks'])
+        mean_tracks = valid_tracks['numTracks'].mean()
+        std_tracks = valid_tracks['numTracks'].std()
+        mean_tracks_pf = (valid_tracks['numTracks'] / valid_tracks['numFrames']).mean()
+        std_tracks_pf = (valid_tracks['numTracks'] / valid_tracks['numFrames']).std()
+
+        summary_txt_path = os.path.join(destination_dir, f'{sample_name}_stats_summary.txt')
+        with open(summary_txt_path, 'w', encoding='utf-8') as f:
+            f.write(f"=== {sample_name} - Summary Statistics ===\n\n")
+
+            f.write(f">> Number of 2D localisations:\n")
+            f.write(f"Mean: {mean_2Dlocs:.2f}\n")
+            f.write(f"Standard Deviation: {std_2Dlocs:.2f}\n")
+            f.write(f"Per frame mean: {mean_2Dlocs_pf:.2f}\nPer frame Std: {std_2Dlocs_pf:.2f}\n\n")
+
+            f.write(f">> Number of raw 3D localisations:\n")
+            f.write(f"Mean: {mean_raw3D:.2f}\n")
+            f.write(f"Standard Deviation: {std_raw3D:.2f}\n")
+            f.write(f"Per frame mean: {mean_raw3D_pf:.2f}\nPer frame Std: {std_raw3D_pf:.2f}\n\n")
+
+            f.write(f">> Number of cropped 3D localisations:\n")
+            f.write(f"Mean: {mean_crop3D:.2f}\n")
+            f.write(f"Standard Deviation: {std_crop3D:.2f}\n")
+            f.write(f"Per frame mean: {mean_crop3D_pf:.2f}\nPer frame Std: {std_crop3D_pf:.2f}\n\n")
+
+            f.write(f">> Number of tracks:\n")
+            f.write(f"Mean: {mean_tracks:.2f}\n")
+            f.write(f"Standard Deviation: {std_tracks:.2f}\n")
+            f.write(f"Per frame mean: {mean_tracks_pf:.2f}\nPer frame Std: {std_tracks_pf:.2f}\n\n")
+        if matlab_results_dir:
+            print(f'Successfully saved "{sample_name}" summary stats to the "../results/{sample_name}_perFOV_results" folder.\n')
+        else:
+            print(f'Successfully saved "{sample_name}" summary stats to the "../results/{sample_name}_analysisSummary" folder.\n')
+
     else:
-        print(f'Reading subfolders, "2D_locs_csv","formatted_3Dlocs","cropped_3Dlocs","tracks", within the "../data" directory to generate analysis summary stats...\n(note: ensure the mentioned subfolders all contain datasets only corresponding to "{sample_name}")')
-        locs2D_dir = os.path.join(root_directory, 'data', '2D_locs_csv')
-        fm_3Dlocs_dir = os.path.join(root_directory, 'data', 'formatted_3Dlocs')
-        cropped_3Dlocs_dir = os.path.join(root_directory, 'data', 'cropped_3Dlocs')
-        tracks_dir = os.path.join(root_directory, 'data', 'tracks')
+        if matlab_results_dir:
+            df = pd.DataFrame({
+                'confPerc': perfConf(matlab_results_dir),
+                'diffusionConst (all)': diffConst(matlab_results_dir),
+                'alpha (all)': anomalous_exp(matlab_results_dir)
+            }).T
+        else: 
+            return
 
-    if matlab_results_dir:
-        df = pd.DataFrame({
-            'numFrames': numFrames(locs2D_dir),
-            'num_2Dlocs': num2D_locs(locs2D_dir),
-            'num_raw3Dlocs': num3D_raw_locs(fm_3Dlocs_dir),
-            'num_cropped3Dlocs': num3D_cropped_locs(cropped_3Dlocs_dir),
-            'numTracks': numTracks(tracks_dir),
-            'confPerc': perfConf(matlab_results_dir),
-            'diffusionConst (all)': diffConst(matlab_results_dir),
-            'alpha (all)': anomalous_exp(matlab_results_dir)
-        }).T
-    else: 
-        df = pd.DataFrame({
-            'numFrames': numFrames(locs2D_dir),
-            'num_2Dlocs': num2D_locs(locs2D_dir),
-            'num_raw3Dlocs': num3D_raw_locs(fm_3Dlocs_dir),
-            'num_cropped3Dlocs': num3D_cropped_locs(cropped_3Dlocs_dir),
-            'numTracks': numTracks(tracks_dir),
-        }).T
+        df.index.name = 'Property'
+        df = df.T
+        df.index.name = 'Sample'
 
-    df.index.name = 'Property'
-    df = df.T
-    df.index.name = 'Sample'
-
-    # calculate per-frame values and add them as new columns
-    df['num_2Dlocs_perframe'] = df['num_2Dlocs'] / df['numFrames']
-    df['num_raw3Dlocs_perframe'] = df['num_raw3Dlocs'] / df['numFrames']
-    df['num_cropped3Dlocs_perframe'] = df['num_cropped3Dlocs'] / df['numFrames']
-    df['numTracks_perframe'] = df['numTracks'] / df['numFrames']
-
-    if matlab_results_dir:
-        destination_dir = os.path.join(root_directory, 'results', sample_name + '_perFOV_results', 'analysisSummary')
-    else:
-        destination_dir = os.path.join(root_directory, 'results', sample_name + '_analysisSummary')
-    os.makedirs(destination_dir, exist_ok=True)
-    csv_destination_path = os.path.join(destination_dir, f'{sample_name}_stats.csv')
-    df.to_csv(csv_destination_path)
-
-    # Per-frame and filtered stats
-    valid_2D = df.dropna(subset=['numFrames', 'num_2Dlocs'])
-    mean_2Dlocs = valid_2D['num_2Dlocs'].mean()
-    std_2Dlocs = valid_2D['num_2Dlocs'].std()
-    mean_2Dlocs_pf = (valid_2D['num_2Dlocs'] / valid_2D['numFrames']).mean()
-    std_2Dlocs_pf = (valid_2D['num_2Dlocs'] / valid_2D['numFrames']).std()
-
-    valid_raw3D = df.dropna(subset=['numFrames', 'num_raw3Dlocs'])
-    mean_raw3D = valid_raw3D['num_raw3Dlocs'].mean()
-    std_raw3D = valid_raw3D['num_raw3Dlocs'].std()
-    mean_raw3D_pf = (valid_raw3D['num_raw3Dlocs'] / valid_raw3D['numFrames']).mean()
-    std_raw3D_pf = (valid_raw3D['num_raw3Dlocs'] / valid_raw3D['numFrames']).std()
-
-    valid_crop3D = df.dropna(subset=['numFrames', 'num_cropped3Dlocs'])
-    mean_crop3D = valid_crop3D['num_cropped3Dlocs'].mean()
-    std_crop3D = valid_crop3D['num_cropped3Dlocs'].std()
-    mean_crop3D_pf = (valid_crop3D['num_cropped3Dlocs'] / valid_crop3D['numFrames']).mean()
-    std_crop3D_pf = (valid_crop3D['num_cropped3Dlocs'] / valid_crop3D['numFrames']).std()
-
-    valid_tracks = df.dropna(subset=['numFrames', 'numTracks'])
-    mean_tracks = valid_tracks['numTracks'].mean()
-    std_tracks = valid_tracks['numTracks'].std()
-    mean_tracks_pf = (valid_tracks['numTracks'] / valid_tracks['numFrames']).mean()
-    std_tracks_pf = (valid_tracks['numTracks'] / valid_tracks['numFrames']).std()
-
-    summary_txt_path = os.path.join(destination_dir, f'{sample_name}_stats_summary.txt')
-    with open(summary_txt_path, 'w', encoding='utf-8') as f:
-        f.write(f"=== {sample_name} - Summary Statistics ===\n\n")
-
-        f.write(f">> Number of 2D localisations:\n")
-        f.write(f"Mean: {mean_2Dlocs:.2f}\n")
-        f.write(f"Standard Deviation: {std_2Dlocs:.2f}\n")
-        f.write(f"Per frame mean: {mean_2Dlocs_pf:.2f}\nPer frame Std: {std_2Dlocs_pf:.2f}\n\n")
-
-        f.write(f">> Number of raw 3D localisations:\n")
-        f.write(f"Mean: {mean_raw3D:.2f}\n")
-        f.write(f"Standard Deviation: {std_raw3D:.2f}\n")
-        f.write(f"Per frame mean: {mean_raw3D_pf:.2f}\nPer frame Std: {std_raw3D_pf:.2f}\n\n")
-
-        f.write(f">> Number of cropped 3D localisations:\n")
-        f.write(f"Mean: {mean_crop3D:.2f}\n")
-        f.write(f"Standard Deviation: {std_crop3D:.2f}\n")
-        f.write(f"Per frame mean: {mean_crop3D_pf:.2f}\nPer frame Std: {std_crop3D_pf:.2f}\n\n")
-
-        f.write(f">> Number of tracks:\n")
-        f.write(f"Mean: {mean_tracks:.2f}\n")
-        f.write(f"Standard Deviation: {std_tracks:.2f}\n")
-        f.write(f"Per frame mean: {mean_tracks_pf:.2f}\nPer frame Std: {std_tracks_pf:.2f}\n\n")
-    if matlab_results_dir:
-        print(f'Successfully saved "{sample_name}" summary stats to the "../results/{sample_name}_perFOV_results" folder.\n')
-    else:
-        print(f'Successfully saved "{sample_name}" summary stats to the "../results/{sample_name}_analysisSummary" folder.\n')
+        destination_dir = os.path.join(root_directory, 'results', sample_name + '_perFOV_results', 'paramValues')
+        os.makedirs(destination_dir, exist_ok=True)
+        csv_destination_path = os.path.join(destination_dir, f'{sample_name}_stats.csv')
+        df.to_csv(csv_destination_path)
 
 
 def correlation_analysis(stats_csv_path, sample_name, root_directory, rgb=(0.5, 0.5, 0.5)):
