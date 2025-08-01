b2efc6242e8e0c61cb0393d2801bdb856bb1d799
Diyan Jain <diyanjain@gmail.com>
2025-08-01 03:04:06 +0100

    updated summarise_analysis.py

--- src/lfm_spt_py/summarise_analysis.py
+++ src/lfm_spt_py/summarise_analysis.py
@@ -22,6 +22,7 @@ def numFrames(input_dir):
             dict_numFrames[dataset_name] = numFrames
     return dict_numFrames
 
+
 def num2D_locs(input_dir):
     df_2D_locs = {}
     if not os.path.exists(input_dir):
@@ -97,7 +98,6 @@ def mean_precision(input_dir):
     return df_latPrecision, df_axPrecision
 
 
-
 def numTracks(input_dir):
     df_tracks = {}
     if not os.path.exists(input_dir):
@@ -129,7 +129,6 @@ def perfConf(matlab_results_dir):
     dict_confPerc = dict(zip(sample_list_editted, confPerc_list))
 
     return dict_confPerc
-
 
 
 def diffConst(matlab_results_dir):
@@ -168,35 +167,66 @@ def anomalous_exp(matlab_results_dir):
     return avg_alpha_dict
 
 
-def compile_stats(sample_name, root_directory, matlab_results_dir=None, custom_dir=None, only_matlab=False):
+def driftMagnitude(matlab_results_dir):
+    csv_path = os.path.join(matlab_results_dir, 'AllTrajectories', 'driftMagnitude.csv')
+    
+    if not os.path.exists(csv_path):
+        raise FileNotFoundError(f"File not found: {csv_path}")
+    
+    df = pd.read_csv(csv_path)
+
+    avg_alpha_dict = {}
+
+    for x in (df['originDataset']).unique():
+        df_perfov = df[df['originDataset'] == x]
+        dataset_name = x.replace('_trackPositions.csv', '')
+        avg_alpha_dict[dataset_name] = np.mean(df_perfov['driftMagnitude'])
+
+    return avg_alpha_dict
+
+
+def Lc(matlab_results_dir):
+    csv_path = os.path.join(matlab_results_dir, 'AllTrajectories', 'Lc.csv')
+    
+    if not os.path.exists(csv_path):
+        raise FileNotFoundError(f"File not found: {csv_path}")
+    
+    df = pd.read_csv(csv_path)
+
+    avg_alpha_dict = {}
+
+    for x in (df['originDataset']).unique():
+        df_perfov = df[df['originDataset'] == x]
+        dataset_name = x.replace('_trackPositions.csv', '')
+        avg_alpha_dict[dataset_name] = np.mean(df_perfov['Lc'])
+
+    return avg_alpha_dict
+
+
+def compile_stats(root_directory, matlab_results_dir=None, custom_data_dir=None, only_matlab=False):
     """ 
-    Compiles statistics (number of 2D locs, 3D locs, tracks, confPerc) per dataset into a summary CSV file.
+    Compiles statistics (number of 2D locs, 3D locs, tracks, confPerc, biophysical_params) per dataset into a CSV file.
     Args:
-        sample_name (str): Name of the sample for labeling the summary.
         root_directory (str): Path to root_directory to read the data folders and saving the summary stats.
         matlab_results_dir (str, optional): Directory containing MATLAB results to include % trajs confined in summary
         custom_data_dir (str, optional): replaces 'root_dir/data' with custom directory path containing the '2D_locs_csv', '3D_fitting_results', 'formatted_3Dlocs', 'cropped_3Dlocs', 'tracks' folders for the given sample.  
         only_matlab (bool, optional): set to True if no data dir input, just confPerc, diffusionConst and alpha.
-    Returns:
-        str: Path to the generated summary CSV file.
     """
     if not only_matlab:
-        if custom_dir:
-            print(f'Reading subfolders, "2D_locs_csv", "3D_fitting_results", "formatted_3Dlocs","cropped_3Dlocs","tracks", within the custom directory as provided to generate analysis summary stats...\n(note: ensure the mentioned subfolders all contain datasets only corresponding to "{sample_name}")')
-            locs2D_dir = os.path.join(custom_dir,'2D_locs_csv')
-            fm_3Dlocs_dir = os.path.join(custom_dir,'formatted_3Dlocs')
-            cropped_3Dlocs_dir = os.path.join(custom_dir, 'cropped_3Dlocs')
-            tracks_dir = os.path.join(custom_dir, 'tracks')
-            fitting3D_dir = os.path.join(custom_dir, '3D_fitting_results')
+        if custom_data_dir:
+            print(f'Reading subfolders, "2D_locs_csv", "3D_fitting_results", "formatted_3Dlocs","cropped_3Dlocs","tracks", within the custom directory as provided to generate analysis summary stats...\n(note: ensure the mentioned subfolders all contain datasets only corresponding to one sample type)')
+            locs2D_dir = os.path.join(custom_data_dir,'2D_locs_csv')
+            fm_3Dlocs_dir = os.path.join(custom_data_dir,'formatted_3Dlocs')
+            cropped_3Dlocs_dir = os.path.join(custom_data_dir, 'cropped_3Dlocs')
+            tracks_dir = os.path.join(custom_data_dir, 'tracks')
+            fitting3D_dir = os.path.join(custom_data_dir, '3D_fitting_results')
         else:
-            print(f'Reading subfolders, "2D_locs_csv","3D_fitting_results", "formatted_3Dlocs","cropped_3Dlocs","tracks", within the "../data" directory to generate analysis summary stats...\n(note: ensure the mentioned subfolders all contain datasets only corresponding to "{sample_name}")')
+            print(f'Reading subfolders, "2D_locs_csv","3D_fitting_results", "formatted_3Dlocs","cropped_3Dlocs","tracks", within the "../data" directory to generate analysis summary stats...\n(note: ensure the mentioned subfolders all contain datasets only corresponding to one sample type)')
             locs2D_dir = os.path.join(root_directory, 'data', '2D_locs_csv')
             fm_3Dlocs_dir = os.path.join(root_directory, 'data', 'formatted_3Dlocs')
             cropped_3Dlocs_dir = os.path.join(root_directory, 'data', 'cropped_3Dlocs')
             tracks_dir = os.path.join(root_directory, 'data', 'tracks')
             fitting3D_dir = os.path.join(root_directory, 'data', '3D_fitting_results')
-
-
 
         if matlab_results_dir:
             df = pd.DataFrame({
@@ -209,7 +239,10 @@ def compile_stats(sample_name, root_directory, matlab_results_dir=None, custom_d
                 'numTracks': numTracks(tracks_dir),
                 'confPerc': perfConf(matlab_results_dir),
                 'diffusionConst (all)': diffConst(matlab_results_dir),
-                'alpha (all)': anomalous_exp(matlab_results_dir)
+                'alpha (all)': anomalous_exp(matlab_results_dir),
+                'driftMagnitude (all)': driftMagnitude(matlab_results_dir),
+                'Lc (all)': Lc(matlab_results_dir)
+
             }).T
         else: 
             df = pd.DataFrame({
@@ -232,12 +265,14 @@ def compile_stats(sample_name, root_directory, matlab_results_dir=None, custom_d
         df['num_cropped3Dlocs_perframe'] = df['num_cropped3Dlocs'] / df['numFrames']
         df['numTracks_perframe'] = df['numTracks'] / df['numFrames']
 
-        if matlab_results_dir:
-            destination_dir = os.path.join(root_directory, 'results', sample_name + '_perFOV_results', 'analysisSummary')
+
+        if custom_data_dir:
+            destination_dir = os.path.join(custom_data_dir, 'analysisMetrics')
         else:
-            destination_dir = os.path.join(root_directory, 'results', sample_name + '_analysisSummary')
+            destination_dir = os.path.join(root_directory, 'data','analysisMetrics')
+        
         os.makedirs(destination_dir, exist_ok=True)
-        csv_destination_path = os.path.join(destination_dir, f'{sample_name}_stats.csv')
+        csv_destination_path = os.path.join(destination_dir, f'analysisSummary.csv')
         df.to_csv(csv_destination_path)
 
         mean_lat_precision = np.mean(df['mean_lateralPrecision'])
@@ -268,9 +303,9 @@ def compile_stats(sample_name, root_directory, matlab_results_dir=None, custom_d
         mean_tracks_pf = (valid_tracks['numTracks'] / valid_tracks['numFrames']).mean()
         std_tracks_pf = (valid_tracks['numTracks'] / valid_tracks['numFrames']).std()
 
-        summary_txt_path = os.path.join(destination_dir, f'{sample_name}_stats_summary.txt')
+        summary_txt_path = os.path.join(destination_dir, f'analysisSummary.txt')
         with open(summary_txt_path, 'w', encoding='utf-8') as f:
-            f.write(f"=== {sample_name} - Summary Statistics ===\n\n")
+            f.write(f"=== Summary Statistics ===\n\n")
 
             f.write(f">> Number of 2D localisations:\n")
             f.write(f"Mean: {mean_2Dlocs:.2f}\n")
@@ -293,10 +328,10 @@ def compile_stats(sample_name, root_directory, matlab_results_dir=None, custom_d
             f.write(f"Mean: {mean_tracks:.2f}\n")
             f.write(f"Standard Deviation: {std_tracks:.2f}\n")
             f.write(f"Per frame mean: {mean_tracks_pf:.2f}\nPer frame Std: {std_tracks_pf:.2f}\n\n")
-        if matlab_results_dir:
-            print(f'Successfully saved "{sample_name}" summary stats to the "../results/{sample_name}_perFOV_results" folder.\n')
+        if custom_data_dir:
+            print(f'Successfully saved analysis summary to the "custom_data_dir/analysisMetrics/" folder.\n')
         else:
-            print(f'Successfully saved "{sample_name}" summary stats to the "../results/{sample_name}_analysisSummary" folder.\n')
+            print(f'Successfully saved analysis summry to the "../data/analysisMetrics" folder.\n')
 
     else:
         if matlab_results_dir:
@@ -312,10 +347,11 @@ def compile_stats(sample_name, root_directory, matlab_results_dir=None, custom_d
         df = df.T
         df.index.name = 'Sample'
 
-        destination_dir = os.path.join(root_directory, 'results', sample_name + '_perFOV_results', 'paramValues')
+        destination_dir = os.path.join(root_directory, 'results', 'paramValues')
         os.makedirs(destination_dir, exist_ok=True)
-        csv_destination_path = os.path.join(destination_dir, f'{sample_name}_stats.csv')
+        csv_destination_path = os.path.join(destination_dir, f'analysisSummary.csv')
         df.to_csv(csv_destination_path)
+        print(f'Successfully saved analysis summry to the "../results/paramValues" folder.\n')
 
 
 def correlation_analysis(stats_csv_path, sample_name, root_directory, rgb=(0.5, 0.5, 0.5)):
